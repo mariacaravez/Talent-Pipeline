6bb07a98c6df4028ee4771fcac32ede6ed971bf6
peterhnijeoma <ijeomapeter55@gmail.com>
2021-02-09 23:07:36 -0800

    change for static files

--- application/index.js
+++ application/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('HELLO!')
-});
+app.use( express.static('public'));
 
 app.listen(6480, () => console.log('Server running on port 6480'));
