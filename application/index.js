2286ae3376470e706d1452c7758c7e6b5e829787
peterhnijeoma <ijeomapeter55@gmail.com>
2021-02-09 23:58:45 -0800

    fixed index.js

--- application/index.js
+++ application/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use( express.static('public'));
+app.use( express.static('forntend/public'));
 
 app.listen(6480, () => console.log('Server running on port 6480'));
