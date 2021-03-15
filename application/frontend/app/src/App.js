af5e5456561c3776c44edfcfe7a7c1b00fb39515
mariacaravez <mcaravez@mailsfsu.edu>
2021-03-15 00:31:58 -0700

    implemented search for json object, will need to use database in next checkpoint

--- application/frontend/app/src/App.js
+++ application/frontend/app/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'; 
-import { Dropdown, Input} from 'semantic-ui-react';
+import { useState } from 'react';
+import { Dropdown, Input, Button} from 'semantic-ui-react';
+import Axios from 'axios';
 // import CardExampleGroups from './search-results.js';
 
 
@@ -9,30 +11,52 @@ const options = [
   { key: 'studentYear', text: 'Student Year', value: 'studentYear' },
   { key: 'gender', text: 'Gender', value: 'gender' },
 ]
-const App = () => {
 
-const [textValue, setTextValue] = React.useState();
+function App() {
 
-  return (
-    <div className="App" textAlign ='center'>
-      <h1>CSC 648 Section 02</h1>
-      <h2>Team 06</h2>
-    
-      <div className="search">
-        <Input type= 'text' placeholder='Search...'
-          value = {textValue}
-          onChange={(e) => {
-          setTextValue(e.targetvalue);
-          }} 
-          action>
-          <input />
-          <Dropdown compact button basic floating options={options} defaultValue='site' />
-          {/* <Link to="/search-results" className="btn btn-primary">Sign up</Link> */}
-          {/* <Button onClick ={<CardExampleGroups/>}>Search</Button> */}
-        </Input>
-      </div>
+const [textValue, setTextValue] = useState("");
+const [studentList, setStudentList] = useState([]);
+
+// const displayInfo = () => {
+//   console.log(textValue);
+// };
+
+const getSearch = () => {
+  Axios.get("http://localhost:6480/search").then((response) => {
+    console.log(response);
+    setStudentList(response.data);
+    // Figure out how to get to next component
+  });
+}
+
+return (
+  <div className='App' textalign ='center'>
+    <h1>CSC 648 Section 02</h1>
+    <h2>Team 06</h2>
+  
+    <div className='search'>
+      <Input type= 'text' placeholder='Search...'
+        value = {textValue}
+        onChange={(e) => {
+        setTextValue(e.target.value);
+        }} 
+        action>
+        <input />
+        <Dropdown compact button basic floating options={options} defaultValue='site' />
+        {/* <Link to="/search-results" className="btn btn-primary">Sign up</Link> */}
+        <Button type='submit' onClick={getSearch}>Search</Button>
+      </Input>
     </div>
-  )
-
-};
+    
+    {studentList.map((val, key) => {
+      return (
+        <div>
+          <h1>{val.name}</h1>
+        <p>{val.major}</p>
+        </div>
+      );
+    })}
+  </div>
+);
+}
 export default App;
