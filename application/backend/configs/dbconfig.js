5d16d9a05fc7c0bf633f24964c12475c59ff2b5e
peterhnijeoma <ijeomapeter55@gmail.com>
2021-03-16 15:14:41 -0700

    initial test

---
+++ application/backend/configs/dbconfig.js
@@ -0,0 +1,10 @@
+//
+// database connection perperties
+//
+
+module.exports = {
+  HOST: "ec2-18-188-8-216.us-east-2.compute.amazonaws.com",
+  USER: "csc648848sp21t6",
+  PASSWORD: "theWayitWorks@SP21",
+  DB: "csc648848db"
+};
--- application/index.js
+++ application/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 //const cors = require('cors');
 
@@ -29,4 +30,16 @@ app.use(express.urlencoded({extended: true}));
 /* ORIGINAL CODE */
 //app.use( express.static('frontend/static'));
 
+
+
+// application landing - root page
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to Milestone Application. The home for direct employment help" });
+});
+
+// make our server application use the routes in routes.js
+require("./application/backend/routes/routes.js")(app);
+
+
+// our server application is lsitening on port 6480
 app.listen(6480, () => console.log('Server running on port 6480'));
