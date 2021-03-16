ed2ac451d9a73570136bb4766523eb012236c936
Bera Coskun <hasanberacoskun@gmail.com>
2021-03-15 23:09:09 -0700

    Implemented frontend search with filtering request with placeholder data

--- application/frontend/app/src/App.js
+++ application/frontend/app/src/App.js
@@ -1,7 +1,7 @@
-import React from 'react'; 
+import React from 'react';
 
 import { useState } from 'react';
-import { Dropdown, Input, Button} from 'semantic-ui-react';
+import { Dropdown, Input, Button, Card, Container} from 'semantic-ui-react';
 import Axios from 'axios';
 
 import './App.css'
@@ -11,22 +11,26 @@ import './App.css'
 const options = [
   { key: 'site', text: 'Entire Site', value: 'site' },
   { key: 'major', text: 'Major', value: 'major' },
-  { key: 'studentYear', text: 'Student Year', value: 'studentYear' },
-  { key: 'gender', text: 'Gender', value: 'gender' },
+  { key: 'standing', text: 'Academic Standing', value: 'standing' },
+  { key: 'gradDate', text: 'Graduation Date', value: 'gradDate' },
 ]
 
 function App() {
 
 const [textValue, setTextValue] = useState("");
 const [studentList, setStudentList] = useState([]);
+const [optionsValue, setOptionsValue] = useState("*");
 
 // const displayInfo = () => {
 //   console.log(textValue);
 // };
 
+
+
 const getSearch = () => {
-  Axios.get("http://localhost:6480/search").then((response) => {
-    console.log(response);
+  Axios.get("http://localhost:6480/search", {params: {textValue: textValue, optionsValue: optionsValue}}).then((response) => {
+    console.log(response.data);
+    console.log(textValue);
     setStudentList(response.data);
     // Figure out how to get to next component
   });
@@ -37,7 +41,7 @@ const getSearch = () => {
 function getSearch() {
   let config = {
     method: 'get',
-    // ROUTE IN BACKEND 
+    // ROUTE IN BACKEND
     url: '/search/getByFilter',
   };
 
@@ -52,53 +56,47 @@ function getSearch() {
 }
 */
 
-/* IN CONTROLLER JS FILE */
-/*
-exports.getAllListings = function (req, res) {
-  Listing.getAllListings(req.visitorId).then(function (listings) {
-      console.log(listings)
-      res.send(listings)
-  })
-  .catch(function (err) {
-      res.send(err)
-  })
-}
-*/
 
-/* IN ROUTER JS FILE */
-/*
-const controller = require('/controller');
-router.get('/search/getByFilter', controller.getByFilter);
-*/
+const handleSelect=(e, data)=>{
+  console.log(data.value);
+  setOptionsValue(data.value);
+  console.log(optionsValue);
+}
 
 return (
-  <div className='App' text-align='center'>
+  <Container fluid className='App' text-align='center' >
     <h1>CSC 648 Section 02</h1>
     <h2>Team 06</h2>
-  
+
     <div className='search'>
-      <Input type= 'text' placeholder='Search...'
+      <Input type= 'text' placeholder='Search Student...'
         value = {textValue}
         onChange={(e) => {
         setTextValue(e.target.value);
-        }} 
+        }}
         action>
         <input />
-        <Dropdown compact button basic floating options={options} defaultValue='site' />
+        <Dropdown compact button basic floating options={options} defaultValue='site'
+          onChange={handleSelect} />
         {/* <Link to="/search-results" className="btn btn-primary">Sign up</Link> */}
         <Button type='submit' onClick={getSearch}>Search</Button>
       </Input>
     </div>
-    
+
     {studentList.map((val, key) => {
       return (
-        <div>
-          <h1>{val.name}</h1>
-        <p>{val.major}</p>
-        </div>
+       <Card centered>
+         <Card.Content header={val.name} />
+    <Card.Content description={val.major} />
+    <Card.Content extra>
+    Graduation Date: {val.graduationDate} 
+    <br></br>
+    Academic Standing: {val.academicStanding}
+    </Card.Content>
+        </Card>
       );
     })}
-  </div>
+  </Container>
 );
 }
 export default App;
