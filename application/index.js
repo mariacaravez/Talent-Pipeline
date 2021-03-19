912160f16062363343383e6c7a4e787581570e02
mariacaravez <mcaravez@mailsfsu.edu>
2021-03-18 21:00:40 -0700

    committing backend for milestone 2

--- application/index.js
+++ application/index.js
@@ -4,56 +4,14 @@ const cors = require('cors');
 const db = require('../application/backend/models/dbaccess.js');
 const path = require('path');
 //const routes = require("../application/backend/routes/routes.js");
+
 app.use('/', express.static(path.join(__dirname, 'frontend/app/build/')))
-// app.use("/");
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
-/* TEST CODE */
-placeholderJson = [
-  {
-    id: 0,
-    name: 'Bera Coskun',
-    major: 'philsophy',
-    academicStanding: 'Junior',
-    graduationDate: '2022',
-    imageName: 'bera.jpg'
-  },
-  {
-    id: 1,
-    name: 'Jeffrey Ye',
-    major: 'computer science',
-    academicStanding: 'Senior',
-    graduationDate: '2021',
-    imageName: 'jeffrey-cropped.jpg'
-  },
-  {
-    id: 2,
-    name: 'Jeffrey Ye',
-    major: 'computer science',
-    academicStanding: 'Senior',
-    graduationDate: '2021',
-    imageName: 'jeffrey-cropped.jpg'
-  },
-  {
-    id: 3,
-    name: 'Jeffrey Ye',
-    major: 'computer science',
-    academicStanding: 'Senior',
-    graduationDate: '2021',
-    imageName: 'jeffrey-cropped.jpg'
-  }
-]
 
-
-//  app.get("/search", (req, res) => {
-//   // console.log(placeholderJson);
-//   console.log(req.query);
-//
-//   res.send(placeholderJson);
-// })
-
-app.get("/test", (req, res) => {
+app.get("/index", (req, res) => {
   console.log(path.join(__dirname, 'frontend/app/public/', 'index.html'));
   res.sendFile(path.join(__dirname, 'frontend/app/build/', 'index.html'));
 })
@@ -124,7 +82,7 @@ app.get("/test", (req, res) => {
    });
 }
 else if (req.query.optionsValue == "site" || req.query.optionsValue == "*") {
- db.query("SELECT * FROM user us, userAttributes ua WHERE us.userTypeID = 1 AND us.userID = ua.userID AND (us.lastName LIKE ? OR us.middleName LIKE ? OR us.firstName LIKE ? OR ua.academicStanding LIKE ? OR ua.gradDate LIKE ? OR AND ua.major LIKE ?)",["%" + req.query.textValue + "%", "%" + req.query.textValue + "%", "%" + req.query.textValue + "%", "%" + req.query.textValue + "%", "%" + req.query.textValue + "%", "%" + req.query.textValue + "%"], (error, result) => {
+ db.query("SELECT * FROM user us, userAttributes ua WHERE us.userTypeID = 1 AND us.userID = ua.userID AND (us.lastName LIKE ? OR us.middleName LIKE ? OR us.firstName LIKE ? OR ua.academicStanding LIKE ? OR ua.gradDate LIKE ? OR ua.major LIKE ?)",["%" + req.query.textValue + "%", "%" + req.query.textValue + "%", "%" + req.query.textValue + "%", "%" + req.query.textValue + "%", "%" + req.query.textValue + "%", "%" + req.query.textValue + "%"], (error, result) => {
    if (error) {
       //console.log("error: ", error);
      res.sendStatus(500);
@@ -136,20 +94,6 @@ else if (req.query.optionsValue == "site" || req.query.optionsValue == "*") {
   });
 }
 })
-
-/* ORIGINAL CODE */
-
-//app.use(express.router);
-
-
-// application landing - root page
-// app.get("/", (req, res) => {
-//   res.json({ message: "Welcome to Milestone Application. The home for direct employment help" });
-// });
-
-// make our server application use the routes in routes.js
-//require("../application/backend/routes/routes.js")(app);
-
 
 // our server application is listening on port 6480
 app.listen(6480, () => console.log('Server running on port 6480'));
